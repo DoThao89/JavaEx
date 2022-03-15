4e0e3093af18a5b03c9c6b684bac367c941e0b0d
DoThao89 <93904825+DoThao89@users.noreply.github.com>
2022-03-15 22:33:05 +0700

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,15 @@
+package session2;
+
+public class Pyramid {
+    public static void main (String[] args){
+        System.out.println("Star Pyramid");
+        System.out.println("============");
+        System.out.println("    *  ");
+        System.out.println("   ***  ");
+        System.out.println("  ***** ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+
+
+    }
+}
